9d6d4b1c63b053affb5db83778f75ffbce273d45
Lucas Labre <lucaslabre@gmail.com>
2022-04-11 22:33:55 -0300

    Classes de domínio iniciadas

---
+++ src/main/java/com/lucas/bookstore/domain/Categoria.java
@@ -0,0 +1,77 @@
+package com.lucas.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+	
+	private Integer id;
+	private String nome;
+	private String Descricao;
+	
+	private List<Livro> livros = new ArrayList<>();
+
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		Descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return Descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
