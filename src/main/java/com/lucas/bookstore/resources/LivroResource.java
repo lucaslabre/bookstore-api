8fe4fd9ce98d2aa90a64955e8f8a20fd253aa8c9
Lucas Labre <lucaslabre@gmail.com>
2022-04-22 20:15:56 -0300

    Listando todos os livros por categoria

--- src/main/java/com/lucas/bookstore/resources/LivroResource.java
+++ src/main/java/com/lucas/bookstore/resources/LivroResource.java
@@ -1,13 +1,18 @@
 package com.lucas.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.bookstore.domain.Livro;
+import com.lucas.bookstore.dtos.LivroDTO;
 import com.lucas.bookstore.services.LivroService;
 
 @RestController
@@ -17,9 +22,16 @@ public class LivroResource {
 	@Autowired
 	private LivroService livroService;
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}") //localhost:8080/livros/1
 	public ResponseEntity<Livro> findById (@PathVariable Integer id) {
 		Livro obj = livroService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping() //localhost:8080/livros?categoria=1
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+		List<Livro> list = livroService.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/lucas/bookstore/services/LivroService.java
+++ src/main/java/com/lucas/bookstore/services/LivroService.java
@@ -1,5 +1,6 @@
 package com.lucas.bookstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,17 @@ public class LivroService {
 	
 	@Autowired
 	private LivroRepository livroRepository;
+	@Autowired
+	private CategoriaService categoriaService;
 	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = livroRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: "+id+", Tipo: "+Livro.class.getName()));
+	}
+	
+	public List<Livro> findAll(Integer id) {
+		categoriaService.findById(id);
+		return livroRepository.findAllByCategoria(id);
 	}
 
 }
