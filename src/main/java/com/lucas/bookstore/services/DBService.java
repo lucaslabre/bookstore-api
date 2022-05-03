bafacee3d2741ffdb56fa67ad3babbf8ebf94683
Lucas Labre <lucaslabre@gmail.com>
2022-05-03 13:35:31 -0300

    Correções de configuração application.properties

--- src/main/java/com/lucas/bookstore/services/DBService.java
+++ src/main/java/com/lucas/bookstore/services/DBService.java
@@ -33,8 +33,8 @@ public class DBService {
 		cat1.getLivros().addAll(Arrays.asList(l1,l2));
 		cat2.getLivros().addAll(Arrays.asList(l3,l4,l5));
 		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+		this.categoriaRepository.saveAllAndFlush(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAllAndFlush(Arrays.asList(l1, l2, l3, l4, l5));
 		
 	}
 	
